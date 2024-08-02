0773553fb27edca36548655b30f4b783fe886b3e
Bartis Hawley-Wall <bartis.hawley-wall@metaco.com>
2024-08-02 09:19:34 -0700

    first round of TDD assumptions

--- tests/entrypoint_behavior.rs
+++ tests/entrypoint_behavior.rs
@@ -4,16 +4,36 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
 
-#[test]
-fn binary_entrypoint_is_invokable() {
-    assert_cmd::Command::cargo_bin("solc")
-        .expect(
-            "The binary should pass compilation and be available at the expected command label.  \
+fn test_command() -> assert_cmd::Command {
+    assert_cmd::Command::cargo_bin("solc").expect(
+        "The binary should pass compilation and be available at the expected command label.  \
              This could fail if the code in the `src` directory could not compile, or if this \
              test code is not correct.  Check the Git blame for both paths to determine the cause \
              of the failure.",
-        )
+    )
+}
+
+#[test]
+fn binary_entrypoint_is_invokable() {
+    test_command()
         .assert()
-        .success()
-        .stdout(predicate::eq("Hello, world!\n"));
+        .append_context(
+            "entrypoint",
+            "The binary should be available at the expected command label.",
+        )
+        .failure()
+        .code(1);
+}
+
+#[test]
+fn invoking_without_any_argument_prints_help() {
+    test_command()
+        .assert()
+        .append_context(
+            "help",
+            "Invoking with no arguments should print the help message.",
+        )
+        .failure()
+        .code(1)
+        .stderr(predicate::str::contains("USAGE:"));
 }
