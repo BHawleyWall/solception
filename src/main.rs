b43888eb2c13860ad25fdb17b69d6243e79ae0c7
Bartis Hawley-Wall <bartis.hawley-wall@metaco.com>
2024-08-02 10:05:43 -0700

    add barebones clap integration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 #![allow(unused_imports, unused_variables, dead_code)]
 
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct Cli {
+    /// Required argument for the target program on Solana
+    program_id: String,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     std::process::exit(0);
 }
