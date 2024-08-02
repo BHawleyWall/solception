3cf5d700bbb16f7c542090b8f0d1e74422a45485
Bartis Hawley-Wall <bartis.hawley-wall@metaco.com>
2024-08-02 08:49:06 -0700

    initial "failing" test; some QoL dev deps added  to support TDD

--- tests/entrypoint_behavior.rs
+++ tests/entrypoint_behavior.rs
@@ -1,1 +1,19 @@
 #![allow(unused_imports, unused_variables, dead_code)]
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
+
+#[test]
+fn binary_entrypoint_is_invokable() {
+    assert_cmd::Command::cargo_bin("solc")
+        .expect(
+            "The binary should pass compilation and be available at the expected command label.  \
+             This could fail if the code in the `src` directory could not compile, or if this \
+             test code is not correct.  Check the Git blame for both paths to determine the cause \
+             of the failure.",
+        )
+        .assert()
+        .success()
+        .stdout(predicate::eq("Hello, world!"));
+}
